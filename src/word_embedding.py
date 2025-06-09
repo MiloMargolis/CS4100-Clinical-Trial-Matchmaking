50eac0ddd04381663ef6c8546b68aebcc506fdc0
ivkendall <isabel@velezkendall.com>
2025-06-09 18:36:15 -0400

    word embedding again

--- src/word_embedding.py
+++ src/word_embedding.py
@@ -6,28 +6,20 @@ import numpy as np
 import pandas as pd
 import re
 
-<<<<<<< Updated upstream
-# for the preprocessing of text
-def preprocess(text):
-    text = text.lower() # converts to lowercase
-    text = re.sub(r'[^a-z\s]', '', text) # removes any characters that are not alphabetical
-    return text.strip().split() # splits it into a list of words
 
-# word2vec usage - to represent each word as a vector
-#chsnge
-=======
 # first preprocess text using simple_preprocess (makes lowercase, cuts non-alphabetical,
 # cuts word of length < 2 and > 50, and turns into list)
 # then trains by turning words in corpus to vectors
->>>>>>> Stashed changes
-def train_word2vec(corpus, vector_size=100, window=5, min_count=1): # can change this ??
-    tokenized_corpus = [simple_preprocess(text, max_len=50) for text in corpus] # preprocess the list of strings (corpus)
+def train_word2vec(corpus, vector_size=100, window=5, min_count=1):  # can change this ??
+    tokenized_corpus = [simple_preprocess(text, max_len=50) for text in corpus]  # preprocess the list of strings (corpus)
     w2v_model = Word2Vec(sentences=tokenized_corpus, vector_size=vector_size, window=window, min_count=min_count)
     return w2v_model
+
 
 # usage example
 # corpus = patients_df["Condition"].tolist() + trials_df["Eligibility"].fillna("").tolist()
 # model = train_word2vec(corpus)
+
 
 def get_weighted_sentence_embedding(text, tfidf_vectorizer, w2v_model):
     # Preprocess text
@@ -56,6 +48,7 @@ def get_weighted_sentence_embedding(text, tfidf_vectorizer, w2v_model):
     embeddings = np.array(embeddings)
     weights = np.array(weights)
     return np.average(embeddings, axis=0, weights=weights)
+
 
 # compute cosine similarity between sentence embeddings?
 def compute_similarity_w2v(patient_text, trial_text, w2v_model, tfidf_vectorizer):
