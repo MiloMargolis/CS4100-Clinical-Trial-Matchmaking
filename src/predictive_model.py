176fa9a02e7c50001f9a2239c7f8c55d3319c385
alvg123 <vuong.a@northeastern.edu>
2025-06-09 18:54:56 -0400

    comments to predict

--- src/predictive_model.py
+++ src/predictive_model.py
@@ -22,22 +22,28 @@ trial_names = ['Trial A', 'Trial B', 'Trial C', 'Trial D', 'Trial E']
 patient = np.array([65, 0, 1, 0])
 
 
+# Will change the distannce to precentage?
+
 def knn(patent_to_predict, trails, trial_names, k):
     distances_list = []
     for trail in trails:
         #getting the distance of the new point to every trail.
         distance = np.linalg.norm(patent_to_predict - trail)
+
+
+        #if clinal trails is one varibale:
+        #distance = np.linalg.norm(patent_to_predict - trails[features])
+
         #putting it in a list
         distances_list.append(distance)
 
-    #sort the distances closest to the new point/pateints first and get the k closest trails 
+    #sort the distances to get the k closest to the new point/pateint  
     nearest_clinical = np.argsort(distances_list)[:k]
     
     #get the trail names, distances/(percentage later), and k top trails
     best_trials = [(trial_names[i], distances_list[i]) for i in nearest_clinical]
 
     return best_trials
-
     
     
 
